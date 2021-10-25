05f14613a44c15f26186c06015db9ccd2d036cee
maaspa <90349318+maaspa@users.noreply.github.com>
2021-10-25 19:38:41 -0400

    Update exercice.py

--- exercice.py
+++ exercice.py
@@ -3,11 +3,30 @@
 
 # TODO: Importez vos modules ici
 
+import random
 
-
+def arbre():
 # TODO: Définissez vos fonction ici
+def ellipsoide(A,B,C,masse_volumique):
+    volume = (4/3 * π * A * B * C)
+    masse = masse_volumique * volume
+    return volume, masse
 
-
+def adn(chaine):
+    assert chaine != []
+    
+    for lettre in chaine:
+        if lettre != ("a" or "t" or "g" or "c"):
+            return False
+        else:
+            return True
+        
+def saisie():
+    lettres = ("a","t","g","c")
+    for i in range (randrange(10):
+        for j in range (len(lettres):
+            sequence_adn += lettres[j] * randrange(10)
+    return sequence_adn
 if __name__ == '__main__':
     # TODO: Appelez vos fonctions ici
 
